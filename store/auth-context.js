f19f37c3dbb55780614eb494002f262b2af7d72a
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2022-12-07 18:10:44 +0530

    authentication context store

---
+++ store/auth-context.js
@@ -0,0 +1,27 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext({
+    token: '',
+    authenticate: () =>{},
+    logout: ()=>{}
+})
+
+export function AuthContextProvider({children}){
+    const [authToken, setAuthToken] = useState()
+
+    function authenticate(token){
+        setAuthToken(token)
+    }
+
+    function logout(){
+        setAuthToken(null)
+    }
+
+    const value = {
+        token: authToken,
+        authenticate: authenticate,
+        logout: logout,
+    }
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}

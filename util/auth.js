e79522c305ff4796fd4c0a28cf0bb69bae0d5ce7
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2022-12-07 19:13:59 +0530

    fixed url string

--- util/auth.js
+++ util/auth.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const API_KEY = "?=AIzaSyDcwndkRSdf8tfZJKMlNU5WztHo0aB552M"
+const API_KEY = "?key=AIzaSyDcwndkRSdf8tfZJKMlNU5WztHo0aB552M"
 
 export async function authenticate(mode, email, password){
     let url = 'https://identitytoolkit.googleapis.com/v1/accounts:'
-    if(mode === "signup") url += "signUp"
-    else url += "signInWithPassword"
+    if(mode === "signup"){
+        url += "signUp"
+    }
+    else{
+        url += "signInWithPassword"
+    }
     url += API_KEY
 
     const repsonse = await axios.post(

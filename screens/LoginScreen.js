7c06938150af113515aa019b2344e99834ecbae7
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2022-12-07 19:09:23 +0530

    added submit handlers and state management

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,20 +1,57 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Alert } from 'react-native'
 import FlatButton from '../components/ui/FlatButton'
 import InputField from '../components/InputField'
 import Title from '../components/ui/Title'
 import { Colors } from '../constants/styles'
 import { useNavigation } from '@react-navigation/native'
+import { useContext, useState } from 'react'
+import { AuthContext } from '../store/auth-context'
+import { authenticate } from '../util/auth'
 
 export default function LoginScreen(){
     const navigation = useNavigation()
+    const authCtx = useContext(AuthContext)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    
+    function stateChange(type, value){
+        switch(type){
+            case "email":
+                setEmail(value)
+                break;
+            case "password":
+                setPassword(value)
+        }
+    }
+
+    async function loginHandler({email, password}){
+        try{
+            const token = await authenticate("login", email, password)
+            authCtx.authenticate(token)
+        }
+        catch(error){
+            Alert.alert("Login Failed", "Check your username and password and try again.")
+        }
+    }
+
     return(
         <View style={styles.container}>
             <Title>Login To Your Account</Title>
             <View>
-                <InputField label="Email" placeholder="someone@example.com"/>
-                <InputField label="Password" secure/>
+                <InputField
+                    label="Email"
+                    placeholder="someone@example.com"
+                    value={email}
+                    changeState={stateChange.bind(this, 'email')}
+                />
+                <InputField
+                    label="Password"
+                    value={password}
+                    secure
+                    changeState={stateChange.bind(this, 'password')}
+                />
             </View>
-            <FlatButton >Login</FlatButton>
+            <FlatButton onPress={()=>loginHandler(email, password)}>Login</FlatButton>
             <FlatButton onPress={()=>{navigation.navigate("Signup")}}>Signup</FlatButton>
         </View>
     )
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,21 +1,69 @@
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Alert } from 'react-native'
 import InputField from '../components/InputField'
 import FlatButton from '../components/ui/FlatButton'
 import Title from '../components/ui/Title'
 import { useNavigation } from '@react-navigation/native'
 import { Colors } from '../constants/styles'
+import { useContext, useState } from 'react'
+import { AuthContext } from '../store/auth-context'
+import { authenticate } from '../util/auth'
 
 export default function SignupScreen(){
     const navigation = useNavigation()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [cnfemail, setCnfemail] = useState("")
+
+    const authCtx = useContext(AuthContext)
+
+    async function signupHandler(email, password){
+        try{
+            const token = await authenticate("signup", email, password)
+            authCtx.authenticate(token) 
+        }
+        catch(error){
+            Alert.alert("Signup Failed", "Try again later")
+        }
+    }
+
+    function stateChange(type, value){
+        switch(type){
+            case "email":
+                setEmail(value)
+                break;
+            case "password":
+                setPassword(value)
+                break;
+            case "cnfmail":
+                setCnfemail(value)
+                break;
+        }
+    }
+
     return(
             <View style={styles.container}>
                 <Title>Sign Up and Get Started</Title>
                 <View>
-                    <InputField label="Email" placeholder="someone@example.com"/>
-                    <InputField label="Confirm Email" placeholder="someone@example.com"/>
-                    <InputField label="Password" secure/>
+                    <InputField
+                        label="Email"
+                        placeholder="someone@example.com"
+                        value={email}
+                        changeState={stateChange.bind(this, 'email')}
+                    />
+                    <InputField
+                        label="Confirm Email"
+                        placeholder="someone@example.com"
+                        value={cnfemail}
+                        changeState={stateChange.bind(this, 'cnfmail')}
+                    />
+                    <InputField
+                        label="Password"
+                        secure
+                        value={password}
+                        changeState={stateChange.bind(this, 'password')}
+                    />
                 </View>
-                <FlatButton >Signup</FlatButton>
+                <FlatButton onPress={()=>signupHandler(email, password)}>Signup</FlatButton>
                 <FlatButton onPress={()=>{navigation.navigate("Login")}}>Login</FlatButton>
             </View>
         

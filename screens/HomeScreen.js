ce5c03aa8de059b0b8f12831f7c4d41f46cd2a0f
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2022-12-12 23:51:58 +0530

    new filter buttons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,10 @@
-import { View, Text, Dimensions } from 'react-native'
+import { View, Text, Dimensions, StyleSheet } from 'react-native'
 import { LineChart } from 'react-native-chart-kit'
 import Title from '../components/ui/Title'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import MyChart from '../components/MyChart'
-import FlatButton from '../components/ui/FlatButton'
+import FilterButton from '../components/graphs/FilterButton'
 
 export default function HomeScreen(){
     const [fetchedData, setFetchedData] = useState(false)
@@ -14,7 +14,7 @@ export default function HomeScreen(){
     const [data, setData] = useState(monthData)
     useEffect(()=>{
         axios.get("http://192.168.1.5:8080/day")
-        .then(response => setDayData({datasets: [{data: response.data.map(item => item.value), color: ()=>'#00ff00'}, {data: response.data.map(item => item.value - 1000), color: ()=> 'black'}], legend:["close", "prediction"]}))
+        .then(response => setDayData({datasets: [{data: response.data.map(item => item.value), color: ()=>'#00ff00'}, {data: response.data.map(item => item.value - 100), color: ()=> 'black'}], legend:["close", "prediction"]}))
         .catch(err => console.log(err))
 
         axios.get("http://192.168.1.5:8080/month")
@@ -24,18 +24,39 @@ export default function HomeScreen(){
         axios.get("http://192.168.1.5:8080/historical")
         .then(response => setHistoricalData({datasets: [{data: response.data.map(item => item.close), color: ()=>'#00ff00'}, {data: response.data.map(item => item.close - 750), color: ()=> 'black'}], legend:["close", "prediction"]}))
         .catch(err => console.log(err))
-
         setFetchedData(true)
+
+        // setInterval(()=>{
+        //     axios.get("http://192.168.1.5:8080/day")
+        //     .then(response => setDayData({datasets: [{data: response.data.map(item => item.value), color: ()=>'#00ff00'}, {data: response.data.map(item => item.value - 1000), color: ()=> 'black'}], legend:["close", "prediction"]}))
+        //     .catch(err => console.log(err))
+        // }, 1080000)
+
+        setInterval(()=>{
+            axios.get("http://192.168.1.5:8080/day")
+            .then(response => setDayData({datasets: [{data: response.data, color: ()=>'#00ff00'}, {data: response.data.map(item => item - 1000), color: ()=> 'black'}], legend:["close", "prediction"]}))
+            .catch(err => console.log(err))
+        }, 30000)
     }
     ,[])
 
     return(
         <View>
-            <Title>SENSEX: {dayData.datasets[0].data[0]}</Title>
+            <Title size={30}>SENSEX: </Title>
+            <Title size={30}>Monthly Prediction: 68310</Title>
             {fetchedData && <MyChart data={data} />}
-            <FlatButton onPress={()=>setData(dayData)}>1 Day</FlatButton>
-            <FlatButton onPress={()=>setData(monthData)}>1 Month</FlatButton>
-            <FlatButton onPress={()=>setData(historicalData)}>historical</FlatButton>
+            <View style={styles.filterContainer}>
+                <FilterButton onPress={()=>setData(dayData)}>1D</FilterButton>
+                <FilterButton onPress={()=>setData(monthData)}>1M</FilterButton>
+                <FilterButton onPress={()=>setData(historicalData)}>5Y</FilterButton>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    filterContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    }
+})

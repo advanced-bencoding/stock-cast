8d1982ee05d7f85b104c1fc6a7c8c608d98dcebf
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2023-01-02 00:05:29 +0530

    added context for pred

--- store/auth-context.js
+++ store/auth-context.js
@@ -4,17 +4,22 @@ export const AuthContext = createContext({
     token: '',
     authenticate: () =>{},
     logout: ()=>{},
+    newPred: ()=>{},
+    pred: 0,
     isAuthenticated: false
 })
 
 export function AuthContextProvider({children}){
     const [authToken, setAuthToken] = useState()
+    const [pred, setPred] = useState(0)
 
     const value = {
         token: authToken,
         authenticate: authenticate,
         isAuthenticated: !!authToken,
         logout: logout,
+        pred: pred,
+        newPred: newPred
     }
 
     function authenticate(token){
@@ -23,6 +28,10 @@ export function AuthContextProvider({children}){
 
     function logout(){
         setAuthToken(null)
+    }
+
+    function newPred(value){
+        setPred(value)
     }
 
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>

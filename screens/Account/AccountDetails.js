934075502960e0261027fe8238de253b80d5ec83
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2023-01-02 00:05:55 +0530

    skeleton with firebase auth access

--- screens/Account/AccountDetails.js
+++ screens/Account/AccountDetails.js
@@ -1,7 +1,23 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+import { Text, View } from "react-native";
+import FlatButton from "../../components/ui/FlatButton";
+import { AuthContext } from "../../store/auth-context";
+import axios from "axios";
+
+const API_KEY = "AIzaSyDcwndkRSdf8tfZJKMlNU5WztHo0aB552M"
 
 export default function AccountDetails(){
+    const authCtx = useContext(AuthContext)
+
+    async function run(){
+        let email = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${API_KEY}`,
+        {idToken: authCtx.token})
+        console.log(email.data)
+    }
+    
     return (
-        <Text>AccountDetails</Text>
+        <View>
+            <Text>AccountDetails</Text>
+        </View>
     )
 }
--- screens/Account/Subscription.js
+++ screens/Account/Subscription.js
@@ -1,6 +1,19 @@
 import { Text } from "react-native";
+import { useContext } from "react";
+import axios from "axios";
+import { AuthContext } from "../../store/auth-context";
+
+const API_KEY = "AIzaSyDcwndkRSdf8tfZJKMlNU5WztHo0aB552M"
 
 export default function Subscription(){
+    const authCtx = useContext(AuthContext)
+
+    async function run(){
+        let email = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${API_KEY}`,
+        {idToken: authCtx.token})
+        console.log(email.data)
+    }
+
     return (
         <Text>Subscription Details</Text>
     )

d264d61f5694043992d110451b654d38aeef8bbf
Benjamin Joseph <benjamin.joseph212620@gmail.com>
2023-01-19 20:19:28 +0530

    email validation function added

--- util/auth.js
+++ util/auth.js
@@ -23,3 +23,18 @@ export async function authenticate(mode, email, password){
 
     return response.data.idToken
 }
+
+export const validate = (values) => {
+    const errors = {}
+  
+    if (!values.email) {
+      errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+      errors.email = 'Invalid email address'
+    }
+  
+    if(!errors.email){
+        return false
+    }
+    return true
+  }

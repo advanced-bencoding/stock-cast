e9f513ce20696d8c7c02d420005a2309af897749
advanced-bencoding <benjamin.joseph212620@gmail.com>
2023-01-28 02:10:47 +0530

    changed segment names to suit nifty indices

--- segments.js
+++ segments.js
@@ -1,14 +1,12 @@
 import Category from './category';
 
 export const SEGMENTS = [
-  new Category('c1', 'Agricultural', '#f5428d'),
   new Category('c2', 'Automobile', '#f54242'),
-  new Category('c3', 'Banking', '#f5a442'),
+  new Category('c3', 'Infrastructure', '#f5a442'),
   new Category('c4', 'Consumer Products', '#f5d142'),
-  new Category('c5', 'Derived Materials', '#368dff'),
   new Category('c6', 'Energy', '#41d95d'),
-  new Category('c7', 'Financial', '#9eecff'),
-  new Category('c8', 'Healthcare', '#b9ffb0'),
+  new Category('c7', 'Financial Services', '#9eecff'),
+  new Category('c8', 'Pharmaceuticals', '#b9ffb0'),
   new Category('c9', 'IT', '#ffc7ff'),
-  new Category('c10', 'Others', '#47fced')
+  new Category('c10', 'Service', '#47fced')
 ];

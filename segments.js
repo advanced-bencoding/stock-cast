2c87180033d5b5f1378cd4dad58ded01986bc5fb
advanced-bencoding <benjamin.joseph212620@gmail.com>
2023-01-28 03:33:04 +0530

    id changed to index name

--- segments.js
+++ segments.js
@@ -1,12 +1,12 @@
 import Category from './category';
 
 export const SEGMENTS = [
-  new Category('c2', 'Automobile', '#f54242'),
-  new Category('c3', 'Infrastructure', '#f5a442'),
-  new Category('c4', 'Consumer Products', '#f5d142'),
-  new Category('c6', 'Energy', '#41d95d'),
-  new Category('c7', 'Financial Services', '#9eecff'),
-  new Category('c8', 'Pharmaceuticals', '#b9ffb0'),
-  new Category('c9', 'IT', '#ffc7ff'),
-  new Category('c10', 'Service', '#47fced')
+  new Category('nifty_auto', 'Automobile', '#f54242'),
+  new Category('nifty_infra', 'Infrastructure', '#f5a442'),
+  new Category('nifty_consum', 'Consumption', '#f5d142'),
+  new Category('nifty_energy', 'Energy', '#41d95d'),
+  new Category('nifty_fin', 'Financial Services', '#9eecff'),
+  new Category('nifty_pharma', 'Pharmaceuticals', '#b9ffb0'),
+  new Category('cnx_it', 'IT', '#ffc7ff'),
+  new Category('nifty_metals', 'Metals', '#47fced')
 ];

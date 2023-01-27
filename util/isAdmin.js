844c1756ec79c9ffa0f707d1ed2b667b151bc8e6
advanced-bencoding <benjamin.joseph212620@gmail.com>
2023-01-27 19:53:20 +0530

    utility to check authorization

--- util/isAdmin.js
+++ util/isAdmin.js
@@ -4,3 +4,7 @@ export async function getRole(email){
     const isAdmin = await axios.get(`https://firestore.googleapis.com/v1/projects/stockcast-8db50/databases/(default)/documents/users/${email}`)
     return isAdmin.data.fields.isAdmin.booleanValue
 }
+
+export async function registerNewUser(email, payload){
+    await axios.post(`https://firestore.googleapis.com/v1/projects/stockcast-8db50/databases/(default)/documents/users?documentId=${email}`, payload)
+}
